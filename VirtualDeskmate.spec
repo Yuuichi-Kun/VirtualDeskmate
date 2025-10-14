090df728370876d6a2d9ca96cd91867c19fd1a07
Yuuichi-Kun <takesyoni76@gmail.com>
2025-10-14 17:48:38 +0700

    Added Launcher Window and Fix some bugs

--- VirtualDeskmate.spec
+++ VirtualDeskmate.spec
@@ -5,7 +5,7 @@ a = Analysis(
     ['main.py'],
     pathex=[],
     binaries=[],
-    datas=[('Dance-Evernight-Unscreen.gif', '.')],
+    datas=[('Icon.png', '.')],
     hiddenimports=[],
     hookspath=[],
     hooksconfig={},
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QSystemTrayIcon, QMenu, QAction, QStyle
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QSystemTrayIcon, QMenu, QAction, QStyle, QPushButton, QFileDialog, QLineEdit, QHBoxLayout, QVBoxLayout
 from PyQt5.QtGui import QMovie, QIcon
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt, QPoint, QSettings
+import weakref
 
 # Helper to get correct path for assets both in dev and PyInstaller bundle
 def resource_path(relative_path):
@@ -13,14 +14,28 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 class CharacterWidget(QWidget):
-    def __init__(self):
+    # Single shared tray icon and references
+    tray_icon = None
+    tray_menu = None
+    action_show = None
+    action_hide = None
+    action_show_launcher = None
+    action_quit = None
+    current_ref = None
+    launcher_ref = None
+
+    def __init__(self, gif_path: str = None, launcher: QWidget = None):
         super().__init__()
+        self.launcher = launcher
+        CharacterWidget.current_ref = weakref.ref(self)
+        if launcher is not None:
+            CharacterWidget.launcher_ref = weakref.ref(launcher)
         
         # --- WINDOW SETUP ---
         self.setWindowFlags(
             Qt.FramelessWindowHint |
             Qt.WindowStaysOnTopHint |
-            Qt.SplashScreen
+            Qt.Tool
         )
         self.setAttribute(Qt.WA_TranslucentBackground)
 
@@ -28,7 +43,8 @@ class CharacterWidget(QWidget):
         self.character_label = QLabel(self)
         
         # Load the GIF
-        gif_path = resource_path('Dance-Evernight-unscreen.gif')
+        if not gif_path:
+            gif_path = resource_path('Dance-Evernight-unscreen.gif')
         self.movie = QMovie(gif_path)
         
         # Enable transparency for the movie
@@ -37,8 +53,9 @@ class CharacterWidget(QWidget):
         # Set the movie on the label
         self.character_label.setMovie(self.movie)
         
-        # Make the label background transparent
+        # Make the label background transparent and pass mouse events to parent for dragging
         self.character_label.setStyleSheet("background: transparent;")
+        self.character_label.setAttribute(Qt.WA_TransparentForMouseEvents, True)
         
         # Start the animation
         self.movie.start()
@@ -56,45 +73,6 @@ class CharacterWidget(QWidget):
         # --- MAKING IT DRAGGABLE ---
         self.old_pos = self.pos()
 
-        # --- SYSTEM TRAY ICON ---
-        if QSystemTrayIcon.isSystemTrayAvailable():
-            self.tray_icon = QSystemTrayIcon(self)
-
-            icon_path = resource_path('Icon.png')
-            icon = QIcon(icon_path) if os.path.exists(icon_path) else QApplication.style().standardIcon(QStyle.SP_ComputerIcon)
-            self.tray_icon.setIcon(icon)
-            self.setWindowIcon(icon)
-            self.tray_icon.setToolTip('VirtualDeskmate')
-
-            tray_menu = QMenu()
-            action_show = QAction('Show', self)
-            action_show.triggered.connect(self.showNormal)
-            action_hide = QAction('Hide', self)
-            action_hide.triggered.connect(self.hide)
-            action_quit = QAction('Quit', self)
-            action_quit.triggered.connect(QApplication.instance().quit)
-
-            tray_menu.addAction(action_show)
-            tray_menu.addAction(action_hide)
-            tray_menu.addSeparator()
-            tray_menu.addAction(action_quit)
-            self.tray_icon.setContextMenu(tray_menu)
-
-            # Left-click tray icon toggles show/hide
-            def on_tray_activated(reason):
-                if reason == QSystemTrayIcon.Trigger:
-                    if self.isHidden():
-                        self.showNormal()
-                    else:
-                        self.hide()
-            self.tray_icon.activated.connect(on_tray_activated)
-            self.tray_icon.show()
-
-    def closeEvent(self, event):
-        # Minimize to tray on close
-        event.ignore()
-        self.hide()
-
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.old_pos = event.globalPos()
@@ -105,11 +83,123 @@ class CharacterWidget(QWidget):
             self.move(self.x() + delta.x(), self.y() + delta.y())
             self.old_pos = event.globalPos()
 
+        # --- SYSTEM TRAY ICON (Singleton) ---
+        if QSystemTrayIcon.isSystemTrayAvailable():
+            if CharacterWidget.tray_icon is None:
+                CharacterWidget.tray_icon = QSystemTrayIcon()
+
+                icon_path = resource_path('Icon.png')
+                icon = QIcon(icon_path) if os.path.exists(icon_path) else QApplication.style().standardIcon(QStyle.SP_ComputerIcon)
+                CharacterWidget.tray_icon.setIcon(icon)
+                self.setWindowIcon(icon)
+                CharacterWidget.tray_icon.setToolTip('VirtualDeskmate')
+
+                CharacterWidget.tray_menu = QMenu()
+                CharacterWidget.action_show = QAction('Show')
+                CharacterWidget.action_hide = QAction('Hide')
+                CharacterWidget.action_show_launcher = QAction('Show Launcher')
+                CharacterWidget.action_quit = QAction('Quit')
+
+                # Wire actions to operate on the current instance/launcher via weakrefs
+                def _show_current():
+                    w = CharacterWidget.current_ref() if CharacterWidget.current_ref else None
+                    if w is not None:
+                        w.showNormal()
+
+                def _hide_current():
+                    w = CharacterWidget.current_ref() if CharacterWidget.current_ref else None
+                    if w is not None:
+                        w.hide()
+
+                def _show_launcher():
+                    l = CharacterWidget.launcher_ref() if CharacterWidget.launcher_ref else None
+                    if l is not None:
+                        l.show()
+                        l.raise_()
+                        l.activateWindow()
+
+                CharacterWidget.action_show.triggered.connect(_show_current)
+                CharacterWidget.action_hide.triggered.connect(_hide_current)
+                CharacterWidget.action_show_launcher.triggered.connect(_show_launcher)
+                CharacterWidget.action_quit.triggered.connect(QApplication.instance().quit)
+
+                CharacterWidget.tray_menu.addAction(CharacterWidget.action_show)
+                CharacterWidget.tray_menu.addAction(CharacterWidget.action_hide)
+                CharacterWidget.tray_menu.addAction(CharacterWidget.action_show_launcher)
+                CharacterWidget.tray_menu.addSeparator()
+                CharacterWidget.tray_menu.addAction(CharacterWidget.action_quit)
+                CharacterWidget.tray_icon.setContextMenu(CharacterWidget.tray_menu)
+
+                def on_tray_activated(reason):
+                    if reason == QSystemTrayIcon.Trigger:
+                        w = CharacterWidget.current_ref() if CharacterWidget.current_ref else None
+                        if w is not None:
+                            if w.isHidden():
+                                w.showNormal()
+                            else:
+                                w.hide()
+                CharacterWidget.tray_icon.activated.connect(on_tray_activated)
+                CharacterWidget.tray_icon.show()
+
+
+class LauncherWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('VirtualDeskmate Launcher')
+        self.setFixedSize(480, 140)
+        self.settings = QSettings('VirtualPartner', 'VirtualDeskmate')
+
+        self.gif_path_input = QLineEdit(self)
+        self.gif_path_input.setPlaceholderText('Choose a GIF (transparent recommended)')
+        last_path = self.settings.value('lastGifPath', type=str)
+        if last_path and os.path.exists(last_path):
+            self.gif_path_input.setText(last_path)
+
+        browse_btn = QPushButton('Browse...', self)
+        show_btn = QPushButton('Show Deskmate', self)
+
+        browse_btn.clicked.connect(self.on_browse)
+        show_btn.clicked.connect(self.on_show)
+
+        path_row = QHBoxLayout()
+        path_row.addWidget(self.gif_path_input)
+        path_row.addWidget(browse_btn)
+
+        root_layout = QVBoxLayout()
+        root_layout.addLayout(path_row)
+        root_layout.addWidget(show_btn)
+
+        self.setLayout(root_layout)
+
+    def on_browse(self):
+        start_dir = os.path.expanduser('~')
+        file_path, _ = QFileDialog.getOpenFileName(self, 'Select GIF', start_dir, 'GIF Files (*.gif)')
+        if file_path:
+            self.gif_path_input.setText(file_path)
+            self.settings.setValue('lastGifPath', file_path)
+
+    def on_show(self):
+        path = self.gif_path_input.text().strip()
+        if not path or not os.path.exists(path):
+            # Fall back to bundled default if invalid
+            path = resource_path('Dance-Evernight-unscreen.gif')
+        else:
+            self.settings.setValue('lastGifPath', path)
+
+        self.deskmate = CharacterWidget(path, launcher=self)
+        self.deskmate.show()
+        self.hide()
+
+    def closeEvent(self, event):
+        # Minimize to tray on close
+        event.ignore()
+        self.hide()
+
 # --- RUN THE APPLICATION ---
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     # Keep app running when all windows are hidden (required for tray-only apps)
     app.setQuitOnLastWindowClosed(False)
-    widget = CharacterWidget()
-    widget.show()
+    launcher = LauncherWindow()
+    launcher.show()
     sys.exit(app.exec_())
